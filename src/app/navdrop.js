58bacecee0635a1f3974c49fe5f13277dc3292bf
jdluis <jdluisdev@gmail.com>
2023-08-19 21:59:57 +0100

    refac vanilla to astro, and create component nav

--- src/app/navdrop.js
+++ src/app/navdrop.js
@@ -1,7 +1,10 @@
 
 function ver(n) {
     document.getElementById("subseccion"+n).style.display="block"
-    }
+    };
+
 function ocultar(n) {
     document.getElementById("subseccion"+n).style.display="none"
     }
+    
+export {ver, ocultar};
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,59 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: { //se pondria la etiqueta que usa tailwind-default
+				secondary: {
+					100: '#E2E2D5',
+					200: '#888883',
+				},
+				palette: {
+					one: '#830A1E',
+					two: '#DE2647',
+					three: '#F3593B',
+					four: '#F19B3E',
+					five: '#F3BD62',
+				},
+				palette2: {
+					one: '#F0EFE9',
+					two: '#F19381',
+					three: '#F1B9A7',
+					four: '#60AA9C',
+					five: '#77C7DE',
+					six: '#B38FB0',
+					text: '#5B5E67'
+				},
+				social: {
+					Facebook: '#3b5998',
+					YouTube: '#bb0000',
+					Twitter: '#55acee',
+					Linkedin: '#007bb5',
+					Instagram: '#E1306C',
+					WhatsApp: '#4dc247',
+					mailchimpBg: '#FFE01B'
+				}
+
+			},
+			fontFamily: {
+				textPrimary: ['Sahitya'],
+				textSecondary: ['Ubuntu']
+			},
+			fontSize: {
+				h1: ['12.5rem']
+			},
+			backgroundImage: {
+				'hero-home': "url('https://i.giphy.com/media/XEb90OzfzTduM/giphy.webp')",
+				'orange': "url(https://i.giphy.com/media/k7K48NJ7qlko8/giphy.webp)",
+				'home2section': "url(/bg2sec.jpg)",
+				'newssection': "url(/bgnews.jpg)",
+			},
+		},
+	},
+	variants: {
+		extend: {
+			backgroundColor: ['active'],
+			opacity: ['active'],
+		},
 	},
 	plugins: [],
 }
